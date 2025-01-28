bdc1f5ccafabbc7f2fcc66f74e18d341908f2589
asorensen <asorensen@cvrx.com>
2025-01-28 16:27:15 -0700

    update to account for removing EID column from tblRates_TM

--- qrys/qryRates_TM.sql
+++ qrys/qryRates_TM.sql
@@ -1,8 +1,8 @@
 -- CREATE VIEW qryRates_TM AS
 SELECT
-    TERRITORY_ID,
-    EID,
-    TIER,
+    TM.TERRITORY_ID,
+    R.REP_EMAIL AS EID,
+    TM.TIER,
     CAST([PLAN] AS MONEY) AS PLAN_FY,
     CAST(THRESHOLD AS MONEY) AS THRESHOLD_FY,
     CAST(THRESHOLD * 0.21 AS MONEY) AS Q1_BL,
@@ -14,4 +14,19 @@ SELECT
     CAST(THRESHOLD * 0.28 AS MONEY) AS Q4_BL,
     CAST([PLAN] * 0.28 AS MONEY) AS Q4_Q
 FROM
-    tblRates_TM
+    tblRates_TM TM
+    LEFT JOIN (
+        SELECT
+            *
+        FROM
+            qryRoster
+        WHERE
+            [isLATEST?] = 1
+            AND role = 'REP'
+            AND REP_EMAIL NOT IN (
+                SELECT
+                    EID
+                FROM
+                    tblRates_RM
+            )
+    ) R ON R.TERRITORY_ID = TM.TERRITORY_ID
--- qrys/qryTerritory_Split.sql
+++ qrys/qryTerritory_Split.sql
@@ -1,82 +1,57 @@
 -- CREATE VIEW qryTerritory_Split AS 
 WITH OPPS AS (
     SELECT
-        *
+        CLOSEDATE,
+        CLOSE_YYYYMM,
+        CLOSE_YYYYQQ,
+        IMPLANTED_DT,
+        IMPLANTED_YYYYMM,
+        IMPLANTED_YYYYQQ,
+        ACCOUNT_INDICATION__C,
+        O.ACT_ID,
+        O.ACT_OWNER_EMAIL,
+        OPP_OWNER_EMAIL,
+        O.OPP_ID,
+        O.NAME,
+        PHYSICIAN,
+        PHYSICIAN_ID,
+        /* first, bring in the email from tblAlign_Opp.
+         If that's null, bring in the email from tblAlign_Act. And finally, if that is null then bring in
+         ACT_OWNER_EMAIL from qryOpps. */
+        ISNULL(
+            ISNULL(
+                AO.EMAIL,
+                AA.OWNER_EMAIL
+            ),
+            O.ACT_OWNER_EMAIL
+        ) AS SALES_CREDIT_REP_EMAIL,
+        INDICATION_FOR_USE__C,
+        REASON_FOR_IMPLANT__C,
+        ISIMPL,
+        IMPLANT_UNITS,
+        REVENUE_UNITS,
+        SALES,
+        ASP
     FROM
-        (
-            SELECT
-                CLOSEDATE,
-                CLOSE_YYYYMM,
-                IMPLANTED_YYYYMM,
-                ACCOUNT_INDICATION__C,
-                O.ACT_ID,
-                O.OPP_ID,
-                O.NAME,
-                PHYSICIAN,
-                PHYSICIAN_ID,
-                OPP_OWNER_EMAIL,
-                /* first, bring in the emails on the splits table. If those are null, bring in the email from tblAlign_Opp.
-                 If that's null, bring in the sales_credit_rep_email from qryOpps.
-                 Finally, if that's null, bring in the AM_FOR_CREDIT_EMAIL from qryOpps. */
-                ISNULL(
-                    ISNULL(
-                        splits.REP_EMAIL,
-                        ISNULL(
-                            AO.EMAIL,
-                            O.SALES_CREDIT_REP_EMAIL
-                        )
-                    ),
-                    O.AM_FOR_CREDIT_EMAIL
-                ) AS SALES_CREDIT_REP_EMAIL,
-                INDICATION_FOR_USE__C,
-                REASON_FOR_IMPLANT__C,
-                ISIMPL,
-                CASE
-                    WHEN splits.SPLIT IS NOT NULL THEN 1
-                    ELSE 0
-                END AS [isSPLIT?],
-                splits.SPLIT,
-                CASE
-                    WHEN splits.SPLIT IS NOT NULL THEN IMPLANT_UNITS * SPLIT
-                    ELSE IMPLANT_UNITS
-                END AS IMPLANT_UNITS,
-                CASE
-                    WHEN splits.SPLIT IS NOT NULL THEN REVENUE_UNITS * SPLIT
-                    ELSE REVENUE_UNITS
-                END AS REVENUE_UNITS,
-                CASE
-                    WHEN splits.SPLIT IS NOT NULL THEN SALES * SPLIT
-                    ELSE SALES
-                END AS SALES,
-                ASP,
-                T.EMAIL AS FCE_PO_EMAIL,
-                T.PO_PER,
-                T.[PO_%]
-            FROM
-                qryOpps O
-                /* check tblAlign_Opp */
-                LEFT JOIN tblAlign_Opp AO ON O.OPP_ID = AO.OPP_ID
-                /* check account splits */
-                LEFT JOIN tblActSplits splits ON O.ACT_ID = splits.ACT_ID
-                AND CLOSE_YYYYMM BETWEEN splits.YYYYMM_ST
-                AND splits.YYYYMM_END
-                /* check FCE payouts */
-                LEFT JOIN tblFCE_TGT_PO T ON CASE
-                    WHEN T.[TYPE] = 'ACCT' THEN O.ACT_ID
-                    WHEN T.[TYPE] = 'DOC' THEN O.PHYSICIAN_ID
-                END = T.OBJ_ID
-                AND O.CLOSE_YYYYMM BETWEEN T.YYYYMM_START
-                AND T.YYYYMM_END
-                AND O.ISIMPL = 1
-            WHERE
-                OPP_STATUS = 'CLOSED'
-                AND SHIPPINGCOUNTRYCODE = 'US'
-                AND INDICATION_FOR_USE__C = 'Heart Failure - Reduced Ejection Fraction'
-        ) A
+        tmpOpps O
+        /* check tblAlign_Opp */
+        LEFT JOIN tblAlign_Opp AO ON O.OPP_ID = AO.OPP_ID
+        /* check tblAlign_Act */
+        LEFT JOIN tblAlign_Act AA ON O.ACT_ID = AA.ACT_ID
+        AND O.CLOSEDATE BETWEEN AA.ST_DT
+        AND AA.END_DT
+    WHERE
+        OPP_STATUS = 'CLOSED'
+        AND SHIPPINGCOUNTRYCODE = 'US'
+        AND INDICATION_FOR_USE__C = 'Heart Failure - Reduced Ejection Fraction'
+        AND REASON_FOR_IMPLANT__C IN ('De novo', 'Replacement')
+        AND STAGENAME = 'Revenue Recognized'
+        AND sales <> 0
 )
 SELECT
     TS.ORIGINAL_OWNER AS OG_OWNER,
     TS.ST_DATE AS SPLIT_START,
+    -- DATEADD(DAY, 270, TS.ST_DATE) AS SPLIT_END,
     CASE
         WHEN OPPS.CLOSEDATE BETWEEN TS.ST_DATE
         AND DATEADD(DAY, 90, TS.ST_DATE) THEN '90%'
