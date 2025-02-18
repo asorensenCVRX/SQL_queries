358c0f4d187cae91521b36b3f16de2c8eeaa9d0f
asorensen <asorensen@cvrx.com>
2025-02-18 10:45:59 -0700

    2025 update

--- MONTHLY TO DO/1 - Monthly_OPP_ALIGN_PROC.sql
+++ MONTHLY TO DO/1 - Monthly_OPP_ALIGN_PROC.sql
@@ -1,88 +1,58 @@
-DECLARE @YYYY_MM AS NVARCHAR(7) = '2024_12';
+DECLARE @YYYY_MM AS NVARCHAR(7) = '2025_01';
 
 
 -- INSERT INTO
 --     tblAlign_Opp
 SELECT
-    --start--
-    A.[ACT_ID],
-    A.[OPP_ID],
-    CASE
-        WHEN c.REP_ID IS NOT NULL THEN c.LNAME_REP
-        ELSE OPP_OWNER_LNAME
-    END AS [L_NAME],
+    ACT_ID,
+    OPP_ID,
+    R.LNAME_REP,
     (
         SELECT
             DISTINCT TOP 1 [Customer #]
         FROM
-            [dbo].[tblAlign_Opp] B
+            [tblAlign_Opp] A
         WHERE
-            B.ACT_ID = A.ACT_ID
+            A.ACT_ID = TM.ACT_ID
     ) AS [CUSTOMER],
     CASE
         WHEN INDICATION_FOR_USE__C = 'Hypertension' THEN 'HTN'
         ELSE 'HF'
     END AS [Indication],
-    CASE
-        WHEN c.REP_ID IS NOT NULL THEN AM_FOR_CREDIT_EMAIL
-        ELSE OPP_OWNER_EMAIL
-    END AS [EID],
-    CASE
-        WHEN c.REP_ID IS NOT NULL THEN c.REP_ID
-        ELSE OPP_OWNER_ID
-    END AS [SFDC_ID] --end--
-    ---Debugging Fields----
-    -- ,
-    --             z.*,
-    --             AM_FOR_CREDIT_EMAIL,
-    --             c.REP_ID [AM_FOR_CREDIT_ID],
-    --             z.Rep,
-    --             a.AM_FOR_CREDIT,
-    --             a.OPP_OWNER_EMAIL,
-    --             a.OPP_OWNER_ID,
-    --             a.ACCOUNT_INDICATION__C,
-    --             a.SALES,
-    --             a.TOTALOPPORTUNITYQUANTITY,
-    --             a.INDICATION_FOR_USE__C,
-    --             a.REASON_FOR_IMPLANT__C,
-    --             a.ACT_OWNER_NAME,
-    --             a.CLOSEDATE,
-    --             b.NAME_REP,
-    --             b.ROLE,
-    --             b.STATUS,
-    --             b.DOT,
-    --             a.SALES,
-    --             a.TOTALOPPORTUNITYQUANTITY,
-    --             ACT_OWNER_EMAIL,
-    --             SALES_CREDIT_REP_EMAIL,
-    --             b.role
+    SALES_CREDIT_REP_EMAIL,
+    R.REP_ID
+    /****** DEBUGGING FIELDS ******/
+-- ,
+--     A.EMAIL AS SFDC_ACCOUNT_OWNER_EMAIL,
+--     TM.OPP_OWNER_EMAIL,
+--     R.DOT AS SALES_CREDIT_REP_DOT
+    /*********************/
 FROM
-    qryOpps A
-    LEFT JOIN qryRoster B ON a.OPP_OWNER_EMAIL = b.REP_EMAIL
-    AND b.[isLATEST?] = 1
-    LEFT JOIN qryRoster C ON a.AM_FOR_CREDIT_EMAIL = C.REP_EMAIL
-    AND c.[isLATEST?] = 1
-    LEFT JOIN tblAlign_Opp Z ON a.OPP_ID = z.OPP_ID
+    qry_COMP_TM_DETAIL TM
+    LEFT JOIN qryRoster R ON R.REP_EMAIL = TM.SALES_CREDIT_REP_EMAIL
+    AND R.[isLATEST?] = 1
+    LEFT JOIN (
+        SELECT
+            A.ID,
+            A.NAME,
+            U.EMAIL
+        FROM
+            sfdcAccount A
+            LEFT JOIN sfdcUser U ON A.OWNERID = U.ID
+    ) A ON A.ID = TM.ACT_ID
 WHERE
-    (
-        CLOSE_YYYYMM = @YYYY_MM
-        AND OPP_COUNTRY = 'US'
-        AND OPP_STATUS = 'CLOSED'
-        AND STAGENAME = 'Revenue Recognized' -- AND ISNULL(sales, 0) <> 0
-    ) ------------Integrity Checks
+    CLOSE_YYYYMM = @YYYY_MM
+    AND CLOSEDATE IS NOT NULL
+    AND STAGENAME = 'Revenue Recognized'
+    /******** INTEGRITY CHECKS *******/
     -- AND (
-    --     OPP_OWNER_EMAIL <> ACT_OWNER_EMAIL
-    --     OR OPP_OWNER_EMAIL <> SALES_CREDIT_REP_EMAIL
-    --     OR OPP_OWNER_EMAIL <> AM_FOR_CREDIT_EMAIL
-    --     OR ACT_OWNER_EMAIL <> SALES_CREDIT_REP_EMAIL
+    --     SALES_CREDIT_REP_EMAIL <> A.EMAIL
+    --     OR SALES_CREDIT_REP_EMAIL <> CASE
+    --         WHEN OPP_OWNER_EMAIL = 'gtemplin@cvrx.com' THEN SALES_CREDIT_REP_EMAIL
+    --         ELSE OPP_OWNER_EMAIL
+    --     END
+    --     OR A.EMAIL <> CASE
+    --         WHEN OPP_OWNER_EMAIL = 'gtemplin@cvrx.com' THEN SALES_CREDIT_REP_EMAIL
+    --         ELSE OPP_OWNER_EMAIL
+    --     END
     -- )
-    -- AND OPP_OWNER_EMAIL <> 'gtemplin@cvrx.com' 
-    ------------Integrity Checks
-    -- AND z.OPP_ID is not null  
-    -- and Z.OPP_ID IN ('0064u00001HnevLAAR')
-    -- and a.OPP_ID = '0064u00001HnevLAAR'
-ORDER BY
-    a.CLOSEDATE,
-    b.region,
-    b.DOT DESC,
-    NAME
