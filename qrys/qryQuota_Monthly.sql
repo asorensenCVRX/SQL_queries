8082a67c5e0c4d4b3a2ef4563993bb873261850c
asorensen <asorensen@cvrx.com>
2025-02-24 11:21:57 -0700

    zero out bl and q for months prior to active_yyyymm

--- qrys/qryQuota_Monthly.sql
+++ qrys/qryQuota_Monthly.sql
@@ -1,56 +1,75 @@
 -- CREATE VIEW qryQuota_Monthly as
-/* add historical baselines and quotas to tblBaseline_Quota_Historical and this query will pull them in */
+/* Update baselines and quotas in tblQuotas and this query will pull them in */
 SELECT
-    CAL.YYYYMM,
-    RATES.YYYYQQ,
-    RATES.TERRITORY_ID,
-    RATES.EID,
+    YYYYMM,
+    YYYYQQ,
+    A.TERRITORY_ID,
+    EID,
     CASE
-        WHEN Q.YYYYQQ IS NULL THEN ISNULL(Q.BASELINE, RATES.THRESHOLD / 3)
-        WHEN Q.YYYYMM IS NULL THEN ISNULL(Q.BASELINE / 3, RATES.THRESHOLD / 3)
+        WHEN YYYYMM < ACTIVE_YYYYMM THEN 0
+        ELSE THRESHOLD
     END AS THRESHOLD,
     CASE
-        WHEN Q.YYYYQQ IS NULL THEN ISNULL(Q.QUOTA, RATES.[PLAN] / 3)
-        WHEN Q.YYYYMM IS NULL THEN ISNULL(Q.QUOTA / 3, RATES.[PLAN] / 3)
+        WHEN YYYYMM < ACTIVE_YYYYMM THEN 0
+        ELSE [PLAN]
     END AS [PLAN]
 FROM
     (
         SELECT
-            YYYYQQ,
-            TERRITORY_ID,
-            EID,
-            THRESHOLD,
-            [PLAN]
+            CAL.YYYYMM,
+            RATES.YYYYQQ,
+            RATES.TERRITORY_ID,
+            RATES.EID,
+            CASE
+                WHEN Q.YYYYQQ IS NULL THEN ISNULL(Q.BASELINE, RATES.THRESHOLD / 3)
+                WHEN Q.YYYYMM IS NULL THEN ISNULL(Q.BASELINE / 3, RATES.THRESHOLD / 3)
+            END AS THRESHOLD,
+            CASE
+                WHEN Q.YYYYQQ IS NULL THEN ISNULL(Q.QUOTA, RATES.[PLAN] / 3)
+                WHEN Q.YYYYMM IS NULL THEN ISNULL(Q.QUOTA / 3, RATES.[PLAN] / 3)
+            END AS [PLAN]
         FROM
-            qryRates_TM_BY_QTR
-        UNION
-        SELECT
-            YYYYQQ,
-            RR.REGION_ID,
-            RBQ.EID,
-            BL,
-            RBQ.QUOTA
-        FROM
-            qryRates_RM_BY_QTR RBQ
-            LEFT JOIN tblRates_RM RR ON RBQ.EID = RR.EID
-    ) AS RATES
-    LEFT JOIN (
-        SELECT
-            DISTINCT YYYYMM,
-            YYYYQQ
-        FROM
-            qryCalendar
-        WHERE
-            LEFT(YYYYQQ, 4) = '2025'
-    ) AS CAL ON RATES.YYYYQQ = CAL.YYYYQQ
-    LEFT JOIN tblQuota Q ON Q.EID = RATES.EID
-    AND (
-        (
-            Q.YYYYMM IS NOT NULL
-            AND Q.YYYYMM = CAL.YYYYMM
-        )
-        OR (
-            Q.YYYYQQ IS NOT NULL
-            AND Q.YYYYQQ = RATES.YYYYQQ
-        )
-    )
+            (
+                SELECT
+                    YYYYQQ,
+                    TERRITORY_ID,
+                    EID,
+                    THRESHOLD,
+                    [PLAN]
+                FROM
+                    qryRates_TM_BY_QTR
+                UNION
+                SELECT
+                    YYYYQQ,
+                    RR.REGION_ID,
+                    RBQ.EID,
+                    BL,
+                    RBQ.QUOTA
+                FROM
+                    qryRates_RM_BY_QTR RBQ
+                    LEFT JOIN tblRates_RM RR ON RBQ.EID = RR.EID
+            ) AS RATES
+            LEFT JOIN (
+                SELECT
+                    DISTINCT YYYYMM,
+                    YYYYQQ
+                FROM
+                    qryCalendar
+                WHERE
+                    LEFT(YYYYQQ, 4) = '2025'
+            ) AS CAL ON RATES.YYYYQQ = CAL.YYYYQQ
+            LEFT JOIN tblQuota Q ON Q.EID = RATES.EID
+            AND (
+                (
+                    Q.YYYYMM IS NOT NULL
+                    AND Q.YYYYMM = CAL.YYYYMM
+                )
+                OR (
+                    Q.YYYYQQ IS NOT NULL
+                    AND Q.YYYYQQ = RATES.YYYYQQ
+                )
+            )
+    ) AS A
+    LEFT JOIN qryRoster R ON A.EID = R.REP_EMAIL
+    AND R.[isLATEST?] = 1
+    AND R.ROLE = 'REP'
