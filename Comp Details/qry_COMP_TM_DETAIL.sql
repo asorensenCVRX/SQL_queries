dabafe0189d262649856cd1d0f50602528d609f4
asorensen <asorensen@cvrx.com>
2025-03-26 09:12:17 -0600

    add hypertension cases to calcs

--- Comp Details/qry_COMP_TM_DETAIL.sql
+++ Comp Details/qry_COMP_TM_DETAIL.sql
@@ -153,7 +153,10 @@ OPPS AS (
     WHERE
         OPP_STATUS = 'CLOSED'
         AND SHIPPINGCOUNTRYCODE = 'US'
-        AND INDICATION_FOR_USE__C = 'Heart Failure - Reduced Ejection Fraction'
+        AND INDICATION_FOR_USE__C IN (
+            'Heart Failure - Reduced Ejection Fraction',
+            'Hypertension'
+        )
         AND (
             CLOSE_YYYY = 2025
             OR IMPLANTED_YYYY = 2025
