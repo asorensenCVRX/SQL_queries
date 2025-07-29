ddce29d7252658c2f629f8d46e883f66068bb693
asorensen <asorensen@cvrx.com>
2025-07-29 13:32:37 -0600

    updates

--- qrys/qryRoster_RM.sql
+++ qrys/qryRoster_RM.sql
@@ -1,24 +1,15 @@
+-- CREATE VIEW qryRoster_RM AS
 SELECT
-    EMP_EMAIL,
-    FNAME,
-    LNAME,
-    NAME,
-    ROLE,
-    Tier,
-    TERRITORY_ID,
-    REGION,
-    START_DT,
-    END_DT,
-    STATUS
+    *
 FROM
     (
         SELECT
             R.EMP_EMAIL,
             E.FNAME,
             E.LNAME,
-            E.NAME,
+            E.FNAME + ' ' + E.LNAME AS NAME,
             R.ROLE,
-            RM.Tier,
+            RM.TIER,
             R.TERRITORY_ID,
             T.REGION,
             R.START_DT,
@@ -26,20 +17,20 @@ FROM
             CASE
                 WHEN END_DT <= GETDATE() THEN 'TERMED'
                 ELSE 'ACTIVE'
-            END AS STATUS
+            END AS [STATUS]
         FROM
-            dbo.tblRoster AS R
-            LEFT OUTER JOIN dbo.qryEmployee AS E ON R.EMP_EMAIL = E.[WORK E-MAIL]
-            LEFT OUTER JOIN dbo.tblRates_RM AS RM ON R.EMP_EMAIL = RM.EID
-            LEFT OUTER JOIN (
+            tblRoster R
+            LEFT JOIN qryEmployee AS E ON R.EMP_EMAIL = E.[WORK E-MAIL]
+            LEFT JOIN tblRates_RM RM ON R.EMP_EMAIL = RM.EID
+            LEFT JOIN (
                 SELECT
                     DISTINCT REGION_ID,
                     REGION
                 FROM
-                    dbo.tblTerritory
-            ) AS T ON T.REGION_ID = R.TERRITORY_ID
+                    tblTerritory
+            ) T ON T.REGION_ID = R.TERRITORY_ID
         WHERE
-            (R.ROLE = 'RM')
+            role = 'RM'
     ) AS A
 WHERE
-    (STATUS = 'ACTIVE')
+    [STATUS] = 'ACTIVE'
