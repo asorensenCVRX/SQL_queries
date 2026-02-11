25b9d2ec0ca91da176257fccdeca5ee2f83a2e2c
Alan Sorensen <asorensen@cvrx.com>
2026-02-11 11:11:43 -0700

    2026 update with new comp logic

--- Comp Summaries/ATM Summary.sql
+++ Comp Summaries/ATM Summary.sql
@@ -1,18 +1,54 @@
+DECLARE @PREV_YYYYMM VARCHAR(7) = FORMAT(DATEADD(MONTH, -1, GETDATE()), 'yyyy_MM');
+
 SELECT
-    CLOSE_YYYYMM,
-    ATM_EMAIL,
-    ATM_NAME,
-    REGION,
-    ISNULL(SUM(IMPLANT_UNITS), 0) AS IMPLANT_UNITS,
-    ISNULL(SUM(REVENUE_UNITS), 0) AS REVENUE_UNITS,
-    ISNULL(SUM(SALES_COMMISSIONABLE), 0) AS SALES_COMMISSIONABLE,
-    ISNULL(SUM(PAYOUT), 0) AS PAYOUT
+    *
+    /******/
+    -- INTO tmpATM_PO
+    /******/
 FROM
-    qry_COMP_ATM_DETAIL
+    (
+        SELECT
+            CLOSE_YYYYMM AS YYYYMM,
+            CLOSE_YYYYQQ AS YYYYQQ,
+            ATM_EMAIL,
+            ATM_NAME,
+            REGION,
+            ISNULL(
+                SUM(
+                    CASE
+                        WHEN IMPLANTED_YYYYMM = @PREV_YYYYMM THEN IMPLANT_UNITS
+                        ELSE 0
+                    END
+                ),
+                0
+            ) AS IMPLANT_UNITS,
+            ISNULL(
+                SUM(
+                    CASE
+                        WHEN CLOSE_YYYYMM = @PREV_YYYYMM THEN REVENUE_UNITS
+                        ELSE 0
+                    END
+                ),
+                0
+            ) AS REVENUE_UNITS,
+            ISNULL(
+                SUM(
+                    CASE
+                        WHEN CLOSE_YYYYMM = @PREV_YYYYMM THEN SALES_COMMISSIONABLE
+                        ELSE 0
+                    END
+                ),
+                0
+            ) AS SALES_COMMISSIONABLE,
+            ISNULL(SUM(PAYOUT), 0) AS PAYOUT
+        FROM
+            qry_COMP_ATM_DETAIL
+        GROUP BY
+            CLOSE_YYYYMM,
+            CLOSE_YYYYQQ,
+            ATM_EMAIL,
+            ATM_NAME,
+            REGION
+    ) AS A
 WHERE
-    CLOSE_YYYYMM = FORMAT(DATEADD(MONTH, -1, GETDATE()), 'yyyy_MM')
-GROUP BY
-    CLOSE_YYYYMM,
-    ATM_EMAIL,
-    ATM_NAME,
-    REGION
+    YYYYMM = @PREV_YYYYMM
