c5b683bbbdf4343c127312327b55fa5975f9acd9
Alan Sorensen <asorensen@cvrx.com>
2026-04-13 20:56:45 -0600

    add logic for calcing rev for TMs with ATM revenue

--- Comp Summaries/TM Summary.sql
+++ Comp Summaries/TM Summary.sql
@@ -200,6 +200,141 @@ detail AS (
         THRESHOLD,
         [PLAN]
 ),
+atm_rep_calcs AS (
+    SELECT
+        A2.EID,
+        FQ_ATM_ACCT_REV AS QTD_ATM_ACCT_REV,
+        M0_ATM_ACCT_REV AS ATM_ACCT_REV,
+        M0_ATM_ACCT_REV * 0.15 AS ATM_ACCT_PO,
+        A2.L1_REV,
+        A2.L1_REV * 0.15 AS L1_PO,
+        A2.L2_REV,
+        A2.L2_REV * 0.2 AS L2_PO,
+        ROUND(
+            (
+                (FQ_ATM_ACCT_REV * 0.15) + (FQ_L1_REV * 0.15) + (FQ_L2_REV * 0.2)
+            ) - TBLPO.QTD_EARNED,
+            2
+        ) AS L1_L2_ATM_PO
+    FROM
+        (
+            SELECT
+                *,
+                CASE
+                    /* SALES_COMMISSIONABLE is inclusive of all sales in the quarter through the last completed month,
+                     and M0_SALES is inclusive of all sales in the month, including sales to ATM targeted accounts */
+                    /* if total sales for the quarter are still less than threshold, return this month's sales minus this month's ATM acct rev */
+                    WHEN SALES_COMMISSIONABLE <= THRESHOLD THEN M0_SALES - M0_ATM_ACCT_REV
+                    /* if total sales for the quarter minus this month's sales are greater than threshold, return 0 */
+                    WHEN SALES_COMMISSIONABLE - M0_SALES > THRESHOLD THEN 0
+                    /* if total sales for the quarter minus this month's sales bring us below threshold, then
+                     return only the sales (non-inclusive of ATM acct sales) that fall below threshold */
+                    WHEN SALES_COMMISSIONABLE - M0_SALES < THRESHOLD THEN CASE
+                        WHEN (THRESHOLD - FQ_ATM_ACCT_REV) - (
+                            (SALES_COMMISSIONABLE - M0_SALES) - (FQ_ATM_ACCT_REV - M0_ATM_ACCT_REV)
+                        ) > 0 THEN (THRESHOLD - FQ_ATM_ACCT_REV) - (
+                            (SALES_COMMISSIONABLE - M0_SALES) - (FQ_ATM_ACCT_REV - M0_ATM_ACCT_REV)
+                        )
+                        ELSE THRESHOLD - FQ_ATM_ACCT_REV
+                    END
+                END AS L1_REV,
+                CASE
+                    WHEN SALES_COMMISSIONABLE <= THRESHOLD THEN 0
+                    WHEN SALES_COMMISSIONABLE - M0_SALES > THRESHOLD THEN M0_SALES - M0_ATM_ACCT_REV
+                    WHEN SALES_COMMISSIONABLE - M0_SALES < THRESHOLD THEN CASE
+                        WHEN (THRESHOLD - FQ_ATM_ACCT_REV) - (
+                            /* sales not including the current month */
+                            (SALES_COMMISSIONABLE - M0_SALES) -
+                            /* ATM acct rev not including the current month */
+                            (FQ_ATM_ACCT_REV - M0_ATM_ACCT_REV)
+                            /* result is sales not including the current month and not including ATM rev */
+                        ) > 0 THEN (M0_SALES - M0_ATM_ACCT_REV) - (
+                            (THRESHOLD - FQ_ATM_ACCT_REV) - (
+                                (SALES_COMMISSIONABLE - M0_SALES) - (FQ_ATM_ACCT_REV - M0_ATM_ACCT_REV)
+                            )
+                        )
+                        ELSE (M0_SALES - M0_ATM_ACCT_REV) - (THRESHOLD - FQ_ATM_ACCT_REV)
+                    END
+                END AS L2_REV,
+                CASE
+                    WHEN SALES_COMMISSIONABLE <= THRESHOLD THEN SALES_COMMISSIONABLE - FQ_ATM_ACCT_REV
+                    WHEN FQ_ATM_ACCT_REV > THRESHOLD THEN 0
+                    ELSE THRESHOLD - FQ_ATM_ACCT_REV
+                END AS FQ_L1_REV,
+                CASE
+                    WHEN SALES_COMMISSIONABLE <= THRESHOLD THEN 0
+                    WHEN SALES_COMMISSIONABLE - FQ_ATM_ACCT_REV = 0 THEN 0
+                    ELSE SALES_COMMISSIONABLE - FQ_ATM_ACCT_REV - CASE
+                        WHEN SALES_COMMISSIONABLE <= THRESHOLD THEN SALES_COMMISSIONABLE - FQ_ATM_ACCT_REV
+                        WHEN FQ_ATM_ACCT_REV > THRESHOLD THEN 0
+                        ELSE THRESHOLD - FQ_ATM_ACCT_REV
+                    END
+                END AS FQ_L2_REV
+            FROM
+                (
+                    SELECT
+                        EID,
+                        SUM(SALES_COMMISSIONABLE) AS SALES_COMMISSIONABLE,
+                        SUM(ATM_ACCOUNT_REVENUE) AS FQ_ATM_ACCT_REV,
+                        SUM(
+                            CASE
+                                WHEN YYYYMM = @YYYYMM THEN SALES_COMMISSIONABLE
+                                ELSE 0
+                            END
+                        ) AS M0_SALES,
+                        SUM(
+                            CASE
+                                WHEN YYYYMM = @YYYYMM THEN ATM_ACCOUNT_REVENUE
+                                ELSE 0
+                            END
+                        ) AS M0_ATM_ACCT_REV,
+                        MAX(THRESHOLD) AS THRESHOLD
+                    FROM
+                        base
+                    WHERE
+                        YYYYQQ = @YYYYQQ
+                        AND YYYYMM <= @YYYYMM
+                    GROUP BY
+                        EID
+                    HAVING
+                        SUM(ATM_ACCOUNT_REVENUE) <> 0
+                ) AS A
+        ) AS A2
+        /* bring in the amount already paid this quarter */
+        LEFT JOIN (
+            SELECT
+                EID,
+                SUM(
+                    CAST(
+                        CASE
+                            WHEN CATEGORY = 'TTL_PO' THEN VALUE
+                        END AS MONEY
+                    )
+                ) - SUM(
+                    CAST(
+                        CASE
+                            WHEN CATEGORY = 'CPAS_PO' THEN VALUE
+                        END AS MONEY
+                    )
+                ) AS QTD_EARNED
+            FROM
+                tblPayout
+            WHERE
+                EID IN (
+                    SELECT
+                        DISTINCT EID
+                    FROM
+                        base
+                    WHERE
+                        ATM_ACCOUNT_REVENUE <> 0
+                        AND YYYYQQ = @YYYYQQ
+                )
+                AND YYYYQQ = @YYYYQQ
+                AND YYYYMM < @YYYYMM
+            GROUP BY
+                EID
+        ) AS TBLPO ON A2.EID = TBLPO.EID
+),
 program_accel AS (
     SELECT
         T.SALES_CREDIT_REP_EMAIL,
@@ -288,17 +423,21 @@ final_data AS (
         D.SALES,
         D.T_SPLIT_SALES,
         D.QTD_SALES,
+        ISNULL(ATM.QTD_ATM_ACCT_REV, 0) AS QTD_ATM_ACCT_REV,
         D.QTD_SALES_MINUS_ATM_REV,
         D.ATM_ACCOUNT_REVENUE,
-        D.L1_REV,
-        D.L2_REV,
+        ISNULL(ATM.L1_REV, D.L1_REV) AS L1_REV,
+        ISNULL(ATM.L2_REV, D.L2_REV) AS L2_REV,
         D.ATM_ACCOUNT_PO,
-        D.L1_PO,
-        D.L2_PO,
+        ISNULL(ATM.L1_PO, D.L1_PO) AS L1_PO,
+        ISNULL(ATM.L2_PO, D.L2_PO) AS L2_PO,
         ISNULL(C.TM_PO, 0) AS CPAS_PO,
         D.IMPLANT_ACCEL_TRUE_UP,
         ISNULL(P.PROGRAM_ACCEL_PO, 0) AS PROGRAM_ACCEL_PO,
-        D.TTL_PO AS BASE_TTL_PO,
+        CASE
+            WHEN ATM.L1_L2_ATM_PO IS NOT NULL THEN ATM.L1_L2_ATM_PO + D.IMPLANT_ACCEL_TRUE_UP
+            ELSE D.TTL_PO
+        END AS BASE_TTL_PO,
         D.IMPLANT_UNITS,
         D.REVENUE_UNITS,
         D.QTD_IMPL_REV_RATIO,
@@ -315,6 +454,7 @@ final_data AS (
         AND P.JOIN_KEY = D.YYYYMM
         LEFT JOIN cpas C ON C.ACCOUNT_OWNER_EMAIL = D.EID
         AND C.CPAS_SUBMIT_YYYYMM = D.YYYYMM
+        LEFT JOIN atm_rep_calcs ATM ON D.EID = ATM.EID
     WHERE
         D.YYYYMM = @YYYYMM
 )
@@ -331,6 +471,7 @@ SELECT
     SALES,
     T_SPLIT_SALES,
     QTD_SALES,
+    QTD_ATM_ACCT_REV,
     QTD_SALES_MINUS_ATM_REV,
     ATM_ACCOUNT_REVENUE,
     L1_REV,
